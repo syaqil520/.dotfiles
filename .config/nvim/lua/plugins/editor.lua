e10480cd1bfa0e2a3fb40c2126bf05c19b40942e
Syaqil Aizat <syaqil520@gmail.com>
2025-06-11 23:59:52 +0800

    [nvim] add guess indent

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -13,7 +13,7 @@ return {
                 update_root = false,
             },
             view = {
-                width = 35,
+                width = 50,
                 preserve_window_proportions = true,
             },
             renderer = {
@@ -87,12 +87,12 @@ return {
         "rachartier/tiny-inline-diagnostic.nvim",
         event = "VeryLazy",
         priority = 1000,
-        enabled = false,
+        enabled = true,
         config = function()
             vim.diagnostic.config({ virtual_text = false })
             -- Default configuration
             require("tiny-inline-diagnostic").setup({
-                preset = "ghost",
+                preset = "modern",
                 options = {
                     -- If multiple diagnostics are under the cursor, display all of them.
                     multiple_diag_under_cursor = true,
@@ -104,5 +104,31 @@ return {
                 },
             })
         end,
+    },
+    {
+        "nmac427/guess-indent.nvim",
+        opts = {
+            auto_cmd = true, -- Set to false to disable automatic execution
+            override_editorconfig = false, -- Set to true to override settings set by .editorconfig
+            filetype_exclude = { -- A list of filetypes for which the auto command gets disabled
+                "netrw",
+                "tutor",
+            },
+            buftype_exclude = { -- A list of buffer types for which the auto command gets disabled
+                "help",
+                "nofile",
+                "terminal",
+                "prompt",
+            },
+            on_tab_options = { -- A table of vim options when tabs are detected
+                ["expandtab"] = false,
+            },
+            on_space_options = { -- A table of vim options when spaces are detected
+                ["expandtab"] = true,
+                ["tabstop"] = "detected", -- If the option value is 'detected', The value is set to the automatically detected indent size.
+                ["softtabstop"] = "detected",
+                ["shiftwidth"] = "detected",
+            },
+        },
     },
 }
