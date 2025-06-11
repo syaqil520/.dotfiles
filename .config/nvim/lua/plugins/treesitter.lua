8275b715d5e9ebc368191802fb3ec4534f2b8c55
Syaqil Aizat <syaqil520@gmail.com>
2025-06-12 00:33:00 +0800

    [nvim] clean up treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -58,8 +58,6 @@ return { -- Highlight, edit, and navigate code
             "http",
             "scss",
             "json5",
-            "markdownlint-cli2",
-            "markdown-toc",
             "prisma",
         },
         -- Autoinstall languages that are not installed
@@ -81,64 +79,5 @@ return { -- Highlight, edit, and navigate code
                 goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
             },
         },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        opts = function()
-            local tsc = require("treesitter-context")
-            Snacks.toggle({
-                name = "Treesitter Context",
-                get = tsc.enabled,
-                set = function(state)
-                    if state then
-                        tsc.enable()
-                    else
-                        tsc.disable()
-                    end
-                end,
-            }):map("<leader>ut")
-            return { mode = "cursor", max_lines = 3 }
-        end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        event = "VeryLazy",
-        enabled = true,
-        config = function()
-            -- If treesitter is already loaded, we need to run config again for textobjects
-            local nvim_treesitter = require("lazy.core.config").plugins["nvim-treesitter"]
-            if nvim_treesitter and nvim_treesitter._.loaded then
-                local Plugin = require("lazy.core.plugin")
-                local opts = Plugin.values(nvim_treesitter, "opts", false)
-                require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
-            end
-
-            -- When in diff mode, we want to use the default
-            -- vim text objects c & C instead of the treesitter ones.
-            local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
-            local configs = require("nvim-treesitter.configs")
-            for name, fn in pairs(move) do
-                if name:find("goto") == 1 then
-                    move[name] = function(q, ...)
-                        if vim.wo.diff then
-                            local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-                            for key, query in pairs(config or {}) do
-                                if q == query and key:find("[%]%[][cC]") then
-                                    vim.cmd("normal! " .. key)
-                                    return
-                                end
-                            end
-                        end
-                        return fn(q, ...)
-                    end
-                end
-            end
-        end,
     },
 }
