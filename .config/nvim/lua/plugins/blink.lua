0f4be0b6ac6819ddbb7553dbc8221a64f967799e
Syaqil Aizat <syaqil520@gmail.com>
2025-06-12 00:20:58 +0800

    [nvim] disable snippets on cmp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,34 +2,33 @@ return {
     { -- Autocompletion
         "saghen/blink.cmp",
         event = "VimEnter",
-        version = "1.*",
         dependencies = {
-            -- Snippet Engine
-            {
-                "L3MON4D3/LuaSnip",
-                version = "2.*",
-                build = (function()
-                    -- Build Step is needed for regex support in snippets.
-                    -- This step is not supported in many windows environments.
-                    -- Remove the below condition to re-enable on windows.
-                    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
-                dependencies = {
-                    -- `friendly-snippets` contains a variety of premade snippets.
-                    --    See the README about individual language/framework/plugin snippets:
-                    --    https://github.com/rafamadriz/friendly-snippets
-                    -- {
-                    --   'rafamadriz/friendly-snippets',
-                    --   config = function()
-                    --     require('luasnip.loaders.from_vscode').lazy_load()
-                    --   end,
-                    -- },
-                },
-                opts = {},
-            },
+            -- -- Snippet Engine
+            -- {
+            --     "L3MON4D3/LuaSnip",
+            --     version = "2.*",
+            --     build = (function()
+            --         -- Build Step is needed for regex support in snippets.
+            --         -- This step is not supported in many windows environments.
+            --         -- Remove the below condition to re-enable on windows.
+            --         if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+            --             return
+            --         end
+            --         return "make install_jsregexp"
+            --     end)(),
+            --     dependencies = {
+            --         -- `friendly-snippets` contains a variety of premade snippets.
+            --         --    See the README about individual language/framework/plugin snippets:
+            --         --    https://github.com/rafamadriz/friendly-snippets
+            --         -- {
+            --         --   'rafamadriz/friendly-snippets',
+            --         --   config = function()
+            --         --     require('luasnip.loaders.from_vscode').lazy_load()
+            --         --   end,
+            --         -- },
+            --     },
+            --     opts = {},
+            -- },
             "folke/lazydev.nvim",
         },
         --- @module 'blink.cmp'
@@ -76,13 +75,13 @@ return {
             },
 
             sources = {
-                default = { "lsp", "path", "snippets", "lazydev" },
+                default = { "lsp", "path", "lazydev" },
                 providers = {
                     lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
                 },
             },
 
-            snippets = { preset = "luasnip" },
+            -- snippets = { preset = "luasnip" },
 
             -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
             -- which automatically downloads a prebuilt binary when enabled.
