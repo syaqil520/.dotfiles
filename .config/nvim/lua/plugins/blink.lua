78a34b83887ca7cfd07c6d572fdae74c97f2c913
Syaqil Aizat <syaqil520@gmail.com>
2025-07-18 00:34:59 +0800

    [nvim] update score blink cmp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,3 +1,5 @@
+local trigger_text = ";"
+
 return {
     { -- Autocompletion
         "saghen/blink.cmp",
@@ -33,8 +35,8 @@ return {
         },
         --- @module 'blink.cmp'
         --- @type blink.cmp.Config
-        opts = {
-            keymap = {
+        opts = function(_, opts)
+            opts.keymap = {
                 -- All presets have the following mappings:
                 -- <tab>/<s-tab>: move to right/left of your snippet expansion
                 -- <c-space>: Open menu or open docs if already open
@@ -48,15 +50,15 @@ return {
 
                 -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
                 --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-            },
+            }
 
-            appearance = {
+            opts.appearance = {
                 -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
                 -- Adjusts spacing to ensure icons are aligned
                 nerd_font_variant = "mono",
-            },
+            }
 
-            completion = {
+            opts.completion = {
                 -- By default, you may press `<c-space>` to show the documentation.
                 -- Optionally, set `auto_show = true` to show the documentation after a delay.
                 documentation = { auto_show = true, auto_show_delay_ms = 500 },
@@ -72,14 +74,95 @@ return {
                     -- Show the ghost text when the menu is closed
                     show_without_menu = true,
                 },
-            },
+            }
 
-            sources = {
-                default = { "lsp", "path", "lazydev" },
+            opts.sources = {
+                default = { "lsp", "path", "lazydev", "snippets" },
                 providers = {
                     lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+                    lsp = {
+                        name = "lsp",
+                        enabled = true,
+                        module = "blink.cmp.sources.lsp",
+                        kind = "LSP",
+                        min_keyword_length = 2,
+                        -- When linking markdown notes, I would get snippets and text in the
+                        -- suggestions, I want those to show only if there are no LSP
+                        -- suggestions
+                        --
+                        -- Enabled fallbacks as this seems to be working now
+                        -- Disabling fallbacks as my snippets wouldn't show up when editing
+                        -- lua files
+                        -- fallbacks = { "snippets", "buffer" },
+                        score_offset = 90, -- the higher the number, the higher the priority
+                    },
+                    path = {
+                        name = "Path",
+                        module = "blink.cmp.sources.path",
+                        score_offset = 25,
+                        -- When typing a path, I would get snippets and text in the
+                        -- suggestions, I want those to show only if there are no path
+                        -- suggestions
+                        fallbacks = { "snippets", "buffer" },
+                        -- min_keyword_length = 2,
+                        opts = {
+                            trailing_slash = false,
+                            label_trailing_slash = true,
+                            get_cwd = function(context)
+                                return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+                            end,
+                            show_hidden_files_by_default = true,
+                        },
+                    },
+                    snippets = {
+                        name = "snippets",
+                        enabled = true,
+                        max_items = 15,
+                        min_keyword_length = 2,
+                        module = "blink.cmp.sources.snippets",
+                        score_offset = 85, -- the higher the number, the higher the priority
+                        -- Only show snippets if I type the trigger_text characters, so
+                        -- to expand the "bash" snippet, if the trigger_text is ";" I have to
+                        -- should_show_items = function()
+                        --     local col = vim.api.nvim_win_get_cursor(0)[2]
+                        --     local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+                        --     -- NOTE: remember that `trigger_text` is modified at the top of the file
+                        --     return before_cursor:match(trigger_text .. "%w*$") ~= nil
+                        -- end,
+                        -- -- After accepting the completion, delete the trigger_text characters
+                        -- -- from the final inserted text
+                        -- -- Modified transform_items function based on suggestion by `synic` so
+                        -- -- that the luasnip source is not reloaded after each transformation
+                        -- -- https://github.com/linkarzu/dotfiles-latest/discussions/7#discussion-7849902
+                        -- -- NOTE: I also tried to add the ";" prefix to all of the snippets loaded from
+                        -- -- friendly-snippets in the luasnip.lua file, but I was unable to do
+                        -- -- so, so I still have to use the transform_items here
+                        -- -- This removes the ";" only for the friendly-snippets snippets
+                        -- transform_items = function(_, items)
+                        --     local line = vim.api.nvim_get_current_line()
+                        --     local col = vim.api.nvim_win_get_cursor(0)[2]
+                        --     local before_cursor = line:sub(1, col)
+                        --     local start_pos, end_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+                        --     if start_pos then
+                        --         for _, item in ipairs(items) do
+                        --             if not item.trigger_text_modified then
+                        --                 ---@diagnostic disable-next-line: inject-field
+                        --                 item.trigger_text_modified = true
+                        --                 item.textEdit = {
+                        --                     newText = item.insertText or item.label,
+                        --                     range = {
+                        --                         start = { line = vim.fn.line(".") - 1, character = start_pos - 1 },
+                        --                         ["end"] = { line = vim.fn.line(".") - 1, character = end_pos },
+                        --                     },
+                        --                 }
+                        --             end
+                        --         end
+                        --     end
+                        --     return items
+                        -- end,
+                    },
                 },
-            },
+            }
 
             -- snippets = { preset = "luasnip" },
 
@@ -90,10 +173,12 @@ return {
             -- the rust implementation via `'prefer_rust_with_warning'`
             --
             -- See :h blink-cmp-config-fuzzy for more information
-            fuzzy = { implementation = "lua" },
+            opts.fuzzy = { implementation = "lua" }
 
             -- Shows a signature help window while you type arguments for a function
             -- signature = { enabled = false, window = { show_documentation = false } },
-        },
+
+            return opts
+        end,
     },
 }
