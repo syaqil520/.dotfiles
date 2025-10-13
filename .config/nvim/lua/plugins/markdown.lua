644e82296d4841f84304d529d9542ee6d4f3fb57
Syaqil Aizat <syaqil520@gmail.com>
2025-10-13 09:24:22 +0800

    [nvim] update markdown file

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -52,12 +52,12 @@ return {
   {
     "obsidian-nvim/obsidian.nvim",
     version = "*",
-    ft = "markdown",
+    -- ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   "BufReadPre " .. vim.fn.expand("~") .. "obsidian/**/*.md",
-    --   "BufNewFile " .. vim.fn.expand("~") .. "obsidian/**/*.md",
-    -- },
+    event = {
+      "BufReadPre " .. vim.fn.expand("~") .. "obsidian/**/*.md",
+      "BufNewFile " .. vim.fn.expand("~") .. "obsidian/**/*.md",
+    },
     ---@module 'obsidian'
     ---@type obsidian.config
     opts = {
