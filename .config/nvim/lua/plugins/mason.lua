5bbd6a83e78444fd34abb24db03a7bfe0ff472ac
Syaqil Aizat <syaqil520@gmail.com>
2026-02-11 22:38:20 +0800

    [nvim] update mason structure

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -4,23 +4,28 @@ return {
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
       "neovim/nvim-lspconfig",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
-    opts = {},
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        automatic_enable = true,
+      })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    cmd = "Mason",
     config = function()
       local lspserver = require("utils.lspserver")
-      require("mason").setup()
-
       local servers = vim.tbl_keys(lspserver.servers or {})
       local ensure_installed = vim.list_extend(servers, lspserver.other or {})
 
       require("mason-tool-installer").setup({
         ensure_installed = ensure_installed,
-      })
-
-      require("mason-lspconfig").setup({
-        automatic_enable = true,
-        ensure_installed = lspserver.servers,
       })
     end,
   },
